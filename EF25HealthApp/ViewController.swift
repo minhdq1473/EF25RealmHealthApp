c95b2cff84aceafec4c36378afc82f3d531f36ff
minh <dqminh0703@gmail.com>
2025-07-04 08:05:36 +0700

    Initial Commit

---
+++ EF25HealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EF25HealthApp
+//
+//  Created by iKame Elite Fresher 2025 on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
